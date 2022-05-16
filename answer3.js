4ee34ef6ae07451a3e76496f5e831c1555c93c97
MikeyTheProgrammer <mikiamin18@gmail.com>
2022-05-16 10:31:29 +0300

    answer 3

---
+++ answer3.js
@@ -0,0 +1,8 @@
+let letterscheck = /["m" || "i" || "d" || "b" || "a" || "r"]/;
+let name = "ly";
+            
+if(letterscheck.test(name)){
+console.log("Great")
+} else {
+    console.log("Bad")
+}
